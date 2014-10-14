da3cd61757d0ce76520945d33160decd7fca5e11
safrulmunajat <safrulmunajat@gmail.com>
2014-10-14 15:23:36 +0700

    commit first

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>FizzBuzz</title>
+	<style type="text/css">
+		table {
+			border-collapse: collapse;
+			width: 30%;
+			text-align: center;
+		}
+		table th {
+			background: yellow;
+		}
+	</style>
+</head>
+<body>
+	<table border="1">
+		<th>Kelipatan 3</th>
+		<th>Kelipatan 5</th>
+		<th>Kelipatan 3 dan 5</th>
+		<tr>
+			<td>
+				<?php
+					$a = 0;
+					while ($a < 100) {
+						$a = $a + 1;
+						if ($a % 3 == 0)
+						{
+							echo "Fizz"."<br>";
+						}
+						else
+						{
+							echo $a."<br>";
+						}
+					}
+				?>
+			</td>
+
+			<td>
+				<?php
+					$b = 0;
+					while ($b < 100) {
+						$b = $b + 1;
+						if ($b % 5 == 0) {
+							echo "Buzz"."<br>";
+						}
+						else {
+							echo $b."<br>";
+						}
+					}
+				?>
+			</td>
+
+			<td>
+				<?php 
+					$c = 0;
+					while ($c < 100) {
+						$c = $c + 1;
+						if ($c % 3 == 0) {
+							echo "Fizz"."<br>";
+						}
+						elseif ($c % 5 == 0) {
+							echo "Buzz"."<br>";
+						}
+						else
+						{
+							echo $c."<br>";
+						}
+					}
+				?>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
